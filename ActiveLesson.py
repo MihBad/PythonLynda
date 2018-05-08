2c507f9bc423ad7f72411c7ac951fd53f19d0216
Mihai Badicioiu <mnbadicioiu@gmail.com>
2018-05-08 19:59:45 +0200

    ch9 les5 - Inheritance

--- ActiveLesson.py
+++ ActiveLesson.py
@@ -361,4 +361,77 @@ def main_9_4():
     # print_animal_9_4(a1)
     print(a1)
     
-if __name__=='__main__': main_9_4()
+# if __name__=='__main__': main_9_4()
+
+# --------------------------------------
+# chapter 9 lesson 5
+# Inheritance
+
+class Animal_9_5:
+    def __init__(self, **kwargs):
+        # initialize the object variables if they exist
+        if 'type' in kwargs:
+            self._type=kwargs['type']
+        if 'name' in kwargs:
+            self._name=kwargs['name']
+        if 'sound' in kwargs:
+            self._sound=kwargs['sound']
+
+    def type(self, t=None):
+        # this is a getter-setter
+        if t:
+            self._type=t
+        try: 
+            return self._type
+        except AttributeError:
+            return None
+
+    
+    def name(self, n=None):
+        # another getter setter
+        if n:
+            self._name=n
+        try:
+            return self._name
+        except AttributeError:
+            return None
+
+    def sound(self, s=None):
+        if s:
+            self._sound=s
+        try: 
+            return self._sound
+        except AttributeError:
+            return None
+
+    def __str__(self):
+        return f'The {self.type()} is named "{self.name()}" and says {self.sound()}'
+
+class Duck_9_5(Animal_9_5):
+    def __init__(self, **kwargs):
+        self._type='duck'
+        if 'type' in kwargs:
+            del kwargs['type']
+        super().__init__(**kwargs)
+
+class Kitten_9_5(Animal_9_5):
+    def __init__(self, **kwargs):
+        self._type='kitten'
+        if 'type' in kwargs:
+            del kwargs['type']
+        super().__init__(**kwargs)
+
+    def kill(self, pTarget):
+        """pTarget (as string) = the target that this (self) kitten will kill, ex: 'human', 'mouse', etc."""
+        print(f'{self.name()} will now kill all {pTarget}!')
+
+def main_9_5():
+    # the code for the chapter 9 lesson 5
+    a0 = Kitten_9_5(name='fluffy', sound='rwar')
+    a1 = Duck_9_5(name='donald', sound='quack')
+    a0.sound('bark')
+    print(a0)
+    print(a1)
+    a0.kill("human")
+    
+if __name__=='__main__': main_9_5()
